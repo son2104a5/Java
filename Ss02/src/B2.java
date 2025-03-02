36e588bdfb6fc6c25a06bba90750334df755f832
kazama679 <147390572+kazama679@users.noreply.github.com>
2025-03-02 21:35:53 +0700

    Update B2.java

--- Ss02/src/B2.java
+++ Ss02/src/B2.java
@@ -8,5 +8,6 @@ public class B2 {
         System.out.println("Hiệu 2 số a - b = " + (a - b));
         System.out.println("Tích 2 số a * b = " + (a * b));
         System.out.println("Thương 2 số a / b = " + (a / b));
+        System.out.println("So du khi chia 2 so a chia cho b = " + (a % b));
     }
 }
