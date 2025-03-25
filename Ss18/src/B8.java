c211117e4523caccb5a155b546aeb689cef065a7
sonnd004 <76640476+son2104a5@users.noreply.github.com>
2025-03-25 12:01:30 +0700

    Update B8.java

--- Ss18/src/B8.java
+++ Ss18/src/B8.java
@@ -10,12 +10,20 @@ public class B8 {
         do {
             System.out.print("Nhập số nguyên dương (1 - 10): ");
             number = Integer.parseInt(sc.nextLine());
-            if (number < 1 || number > 10) {
+            if (number < 1 || number > 3999) {
                 System.out.println("Số không hợp lệ. Vui lòng nhập lại.");
             }
-        } while (number < 1 || number > 10);
+        } while (number < 1 || number > 3999);
 
         LinkedHashMap<Integer, String> romanMap = new LinkedHashMap<>();
+        romanMap.put(1000, "M");
+        romanMap.put(900, "CM");
+        romanMap.put(500, "D");
+        romanMap.put(400, "CD");
+        romanMap.put(100, "C");
+        romanMap.put(90, "XC");
+        romanMap.put(50, "L");
+        romanMap.put(40, "XL");
         romanMap.put(10, "X");
         romanMap.put(9, "IX");
         romanMap.put(5, "V");
